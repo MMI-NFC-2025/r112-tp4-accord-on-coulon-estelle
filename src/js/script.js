a578aefb0b4a1814201335c665b35390adae8e3d
coulon-estelle <estelle.coulon@edu.univ-fcomte.fr>
2025-11-25 14:49:09 +0100

    update README with name, add CSS styles for accordion, and implement accordion functionality in JavaScript

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
-// *** ACCORDEON ***
+const accordeon = document.querySelector(".accordeon");
+const details = accordeon.querySelectorAll("details");
+
+details.forEach(det => {
+  det.querySelector("summary").addEventListener("click", event => {
+
+    // Fermer tous les autres
+    details.forEach(other => {
+      if (other !== det) {
+        other.open = false;
+      }
+    });
+  });
+});
